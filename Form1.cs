0e5f3d5133ce7dc0688a99c8452e65cd46422037
山本富也 <71494868+YamamotoTomiya@users.noreply.github.com>
2022-05-23 02:11:15 +0900

    Add files via upload
    Arduinoモニター

--- Form1.cs
+++ Form1.cs
@@ -7,177 +7,223 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Dynamic;
+using System.IO.Ports;
+using System.Threading;
 using System.IO;
-using System.Windows;
+using System.Windows.Forms.DataVisualization.Charting;
 
-namespace kabetosi
+
+namespace ArduinoMonitor
 {
     public partial class Form1 : Form
     {
-        Vector[] blockSpeed1 = new Vector[10];
-        Vector[] blockPos1 = new Vector[10];
-        Vector[] blockSpeed2 = new Vector[10];
-        Vector[] blockPos2 = new Vector[10];
-        Vector[] ballPos = new Vector[2];
-
-        Random r = new Random();
-
-        int r1 = 0;
-        int r2 = 0;
-        int r3 = 0;
-
-        int a = 0;
-        int c = 0;
-        int e = 0;
-
-        Timer timer1 = new Timer();
+        private ManualResetEvent IsNotReading = new ManualResetEvent(true);
+        String[] separator_newline = new string[] { "\r\n" };
+        Series[] serial_rcv_msg;
         public Form1()
         {
             InitializeComponent();
-
-            r1 = r.Next(250);
-            r2 = r.Next(250);
-            r3 = r.Next(250);
-
-            a = r1 + 60;
-            c = r2 + 60;
-            e = r3 + 60;
-
-            this.ballPos[1] = new Vector(20, 100);
-
-            this.blockPos1[1] = new Vector(Width, 0);//壁の位置
-            this.blockSpeed1[1] = new Vector(-2, 0);
-            this.blockPos2[1] = new Vector(Width, a);//壁の位置
-            this.blockSpeed2[1] = new Vector(-2, 0);
-
-            this.blockPos1[2] = new Vector(Width - 170, 0);//壁の位置
-            this.blockSpeed1[2] = new Vector(-2, 0);
-            this.blockPos2[2] = new Vector(Width - 170, c);//壁の位置
-            this.blockSpeed2[2] = new Vector(-2, 0);
-
-            this.blockPos1[3] = new Vector(Width - 340, 0);//壁の位置
-            this.blockSpeed1[3] = new Vector(-2, 0);
-            this.blockPos2[3] = new Vector(Width - 340, e);//壁の位置
-            this.blockSpeed2[3] = new Vector(-2, 0);
-
-            timer1.Interval = 20;
-            timer1.Tick += new EventHandler(Update1);
-            timer1.Start();
-
+            InitializeSerialComponent();
         }
-        private void Update1(object sender, EventArgs e)
+        private void InitializeSerialComponent()
         {
+            foreach (string portName in SerialPort.GetPortNames())
+            {
+                comboBox_COMselect.Items.Add(portName);
+            }
+            if (comboBox_COMselect.Items.Count > 0)
+            {
+                comboBox_COMselect.SelectedIndex = 0;
+                comboBox_COMselect.Enabled = true;
+                comboBox_baudrate.Enabled = true;
+                comboBox_newline.Enabled = true;
+                button_serial_conn.Enabled = true;
 
-            // ボールの移動
-            blockPos1[1] += blockSpeed1[1];
-            blockPos2[1] += blockSpeed2[1];
-            blockPos1[2] += blockSpeed1[2];
-            blockPos2[2] += blockSpeed2[2];
-            blockPos1[3] += blockSpeed1[3];
-            blockPos2[3] += blockSpeed2[3];
-
-            if (blockPos2[1].Y - 40 > ballPos[1].Y && blockPos1[1].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
             }
-            if (blockPos2[1].Y < ballPos[1].Y && blockPos1[1].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
-            }
-            if (blockPos2[2].Y - 40 > ballPos[1].Y && blockPos1[2].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
-            }
-            if (blockPos2[2].Y < ballPos[1].Y && blockPos1[2].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
-            }
-            if (blockPos2[3].Y - 40 > ballPos[1].Y && blockPos1[3].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
-            }
-            if (blockPos2[3].Y < ballPos[1].Y && blockPos1[3].X == ballPos[1].X + 20)
-            {
-                timer1.Stop();
-            }
-            if (blockPos1[1].X < 0)
-            {
-                blockPos1[1].X = Width;
-                blockPos2[1].X = Width;
-                int r4 = r.Next(250);
-                int a2 = 0;
-                a2 = r4 + 60;
-                blockPos2[1].Y = a2;
-            }
-            if (blockPos1[2].X < 0)
-            {
-                blockPos1[2].X = Width;
-                blockPos2[2].X = Width;
-                int r5 = r.Next(250);
-                int c2 = 0;
-                c2 = r5 + 60;
-                blockPos2[2].Y = c2;
-            }
-            if (blockPos1[3].X < 0)
-            {
-                blockPos1[3].X = Width;
-                blockPos2[3].X = Width;
-                int r6 = r.Next(250);
-                int e2 = 0;
-                e2 = r6 + 60;
-                blockPos2[3].Y = e2;
-            }
-            // 再描画
-            Invalidate();
         }
-        private void Form1_Load(object sender, EventArgs e)
+        
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+       
+        }
+
+        private void comboBox4_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void Draw(object sender, PaintEventArgs e)
+        private void groupBox2_Enter(object sender, EventArgs e)
         {
 
-            SolidBrush pinkBrush = new SolidBrush(Color.LightPink);
-            SolidBrush blueBrush = new SolidBrush(Color.OrangeRed);
-
-            float bx1 = (float)this.ballPos[1].X;
-            float by1 = (float)this.ballPos[1].Y;
-
-            float px1 = (float)this.blockPos1[1].X;
-            float py1 = (float)this.blockPos1[1].Y;
-            float px2 = (float)this.blockPos2[1].X;
-            float py2 = (float)this.blockPos2[1].Y;
-            float px3 = (float)this.blockPos1[2].X;
-            float py3 = (float)this.blockPos1[2].Y;
-            float px4 = (float)this.blockPos2[2].X;
-            float py4 = (float)this.blockPos2[2].Y;
-            float px5 = (float)this.blockPos1[3].X;
-            float py5 = (float)this.blockPos1[3].Y;
-            float px6 = (float)this.blockPos2[3].X;
-            float py6 = (float)this.blockPos2[3].Y;
-
-            e.Graphics.FillRectangle(pinkBrush, bx1, by1, 20, 20);
-
-            e.Graphics.FillRectangle(blueBrush, px1, py1, 5, py2 - 40);
-            e.Graphics.FillRectangle(blueBrush, px2, py2, 5, 1000);
-            e.Graphics.FillRectangle(blueBrush, px3, py3, 5, py4 - 40);
-            e.Graphics.FillRectangle(blueBrush, px4, py4, 5, 1000);
-            e.Graphics.FillRectangle(blueBrush, px5, py5, 5, py6 - 40);
-            e.Graphics.FillRectangle(blueBrush, px6, py6, 5, 1000);
         }
 
-        private void KeyPressed(object sender, KeyPressEventArgs e)
+        private void button_serial_conn_Click(object sender, EventArgs e)
+        {
+            if (button_serial_conn.Enabled==true)
+            {
+                serialPort_Arduino.PortName = comboBox_COMselect.SelectedItem.ToString();
+                serialPort_Arduino.BaudRate = int.Parse(comboBox_baudrate.SelectedItem.ToString());
+                switch (comboBox_newline.SelectedItem.ToString())
+                {
+                    case "GR+LF":
+                        serialPort_Arduino.NewLine = "\r\n";
+                        break;
+
+                    case "CR":
+                        serialPort_Arduino.NewLine = "\r";
+                        break;
+
+                    case "LF":
+                        serialPort_Arduino.NewLine = "\n";
+                        break;
+
+                    default:
+                        return;
+
+                }
+                serialPort_Arduino.Parity = System.IO.Ports.Parity.None;
+                serialPort_Arduino.DataBits = 8;
+                serialPort_Arduino.StopBits = System.IO.Ports.StopBits.One;
+                serialPort_Arduino.Handshake = System.IO.Ports.Handshake.RequestToSend;
+                serialPort_Arduino.Encoding = Encoding.UTF8;
+                serialPort_Arduino.DtrEnable = true;
+
+                System.Threading.Thread.Sleep(500);
+                serialPort_Arduino.Open();
+                serialPort_Arduino.DiscardInBuffer();
+                button_serial_conn.Enabled = false;
+                button_serial_disconn.Enabled = true;
+                button_send_msg.Enabled = true;
+
+                label_serial_status.Text = "connected";
+
+            }
+        }
+
+        private void serialPort_Arduino_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (serialPort_Arduino.IsOpen == false)
+            {
+                return;
+            }
+            try
+            {
+                this.IsNotReading.Reset();
+                string data = serialPort_Arduino.ReadLine();
+                if (!string.IsNullOrEmpty(data))
+                {
+                    BeginInvoke((MethodInvoker)(() =>
+                    {
+                        textBox_rcv_msg.AppendText(data + "\r\n");
+                    }));
+                    String[] splitted = data.Split(separator_newline, StringSplitOptions.RemoveEmptyEntries);
+                    String[] digits = splitted[0].Split(',');
+                    int numeric_count = 0;
+                    for(int j=0;j<digits.Length;j++)
+                    {
+                        if(double.TryParse(digits[j],out double tmp_digits))
+                        {
+                            numeric_count++;
+                        }
+                    }
+                    B
+                }
+                this.IsNotReading.Set();
+                Thread.Sleep(50);
+            }
+            catch (InvalidOperationException)
+            {
+                return;
+            }
+        }
+
+        private void label8_TextChanged(object sender, EventArgs e)
         {
 
-            if (e.KeyChar == 'a')
+        }
+
+        private void button_serial_disconn_Click(object sender, EventArgs e)
+        {
+            if (serialPort_Arduino.IsOpen == true)
             {
-                this.ballPos[1].Y += 10;
+                this.IsNotReading.WaitOne();
+                serialPort_Arduino.Close();
+                button_serial_conn.Enabled = true;
+                button_serial_disconn.Enabled = false;
+                button_send_msg.Enabled = false;
+                label_serial_status.Text = "disconnected";
+
             }
-            else if (e.KeyChar == 'd')
+        }
+
+        private void button_send_msg_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox_send_msg.Text))
             {
-                this.ballPos[1].Y -= 10;
+                Serial_send_msg(textBox_send_msg.Text);
+                textBox_send_msg.Text = null;
+                textBox_rcv_msg.Focus();
+            }
+        }
+        private void Serial_send_msg(string msg)
+        {
+            try
+            {
+                serialPort_Arduino.WriteLine(msg);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "送信エラー",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void textBox_send_msg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Serial_send_msg(textBox_rcv_msg.Text);
+                textBox_send_msg.Text = null;
+                textBox_rcv_msg.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.FileName = "新しいファイル.txt";
+            sfd.InitialDirectory = @"C:\";
+            sfd.Filter = "テキスト　ファイル(*.txt)|*.txt|すべてのファイル(*.*)|*.*";
+            sfd.Title = "保存先のファイルを選択してください";
+            sfd.RestoreDirectory = true;
+
+            sfd.ShowDialog();
+
+            StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.ASCII);
+            sw.Write(textBox_rcv_msg.Text);
+            sw.Close();
+            MessageBox.Show("Saved.");
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+
+        }
+
+        private void textBox_send_msg_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort_Arduino.IsOpen)
+            {
+                this.IsNotReading.WaitOne();
+                serialPort_Arduino.Close();
             }
         }
     }
